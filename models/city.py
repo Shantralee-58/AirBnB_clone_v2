086a3ffe2492d9191eb9d661938218a1242533ec
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-01-25 00:43:23 -0800

    city.py redo

--- models/city.py
+++ models/city.py
@@ -7,17 +7,15 @@ from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    __tablename__ = 'cities'
+    __tablename__ = "cities"
 
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
 
-    places = relationship("Place", backref="cities",
-                          cascade="all, delete-orphan")
+    places = relationship("Place", backref="cities", cascade="all, delete-orphan")
 
     def __init__(self, *args, **kwargs):
-        """ Initiate the instance with some default values and call the
+        """Initiate the instance with some default values and call the
         super init to complete the initiate
         """
         super().__init__(*args, **kwargs)
-        
