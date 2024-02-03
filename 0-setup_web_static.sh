da83b22dc593e327f463a05ac9d2041eebff9776
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-02-03 05:27:15 -0800

    fix first task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
-# This script sets up the web servers for the deployment of web_static.
-
-# Install Nginx if not already installed
+# script that sets up web servers for the deployment of web_static
 sudo apt-get update
 sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# Create necessary folders
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create symbolic link
-sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Set ownership
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "/location \/hbnb_static {/ {n; s/alias.*/alias \/data\/web_static\/current\/;/}" /etc/nginx/sites-available/default
 
-# Restart Nginx
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
 sudo service nginx restart
