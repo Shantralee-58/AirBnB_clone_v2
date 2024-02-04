0bac9110aed3f157d012dfdfbd8587811ef7e8b9
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-02-04 13:06:59 -0800

    task 2 redo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,6 +25,11 @@ def do_deploy(archive_path):
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+
+        if run('echo "Hello"').failed:
+           return False
+
         return True
-    except:
+    except Exception as e:
+        print(e)
         return False
